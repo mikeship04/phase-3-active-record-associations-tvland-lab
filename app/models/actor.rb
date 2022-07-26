b29950ba528a2a42f6af17de449a3cfec4bfe20a
Mike Shippy <mikeship04@gmail.com>
2022-07-26 16:57:38 -0700

    commit tree 517a27cd87b69d3b4710886d60b69f4598d1fba2

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    #we want an array containing a string with both the character and show
+    roles = self.characters
+    roles.map {|a| "#{a.name} - #{a.show.name}"}
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
+
+  def actors_list
+    cast = self.characters
+    cast.map {|a| "#{a.actor.first_name} #{a.actor.last_name}"}
+
+    #characters.map{|c| "#{c.actor.full_name}"}
+  end
 end
